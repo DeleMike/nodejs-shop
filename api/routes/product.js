75e23e0deaa858acf84f1c9ad7376a90b3394ce7
DeleMike <akindelemichael65@gmail.com>
2021-02-12 13:38:33 +0100

    create product routes

---
+++ api/routes/product.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+//enables us to access this resource "/products"
+const router = express.Router(); 
+
+//create the GET request method for this route
+router.get('/');
