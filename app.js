17aa73b5da4ce45db04f43e9ca5ec5299d7350c5
DeleMike <akindelemichael65@gmail.com>
2021-02-12 15:06:41 +0100

    tackle error-handling

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 //import express library
 const express = require('express');
+const morgan = require('morgan');
 
 //save all functions that comes with "express" in a variable to access its functions
 const app = express();
 
+//add app routes
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders'); 
 
+app.use(morgan('dev')); //logging
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next)=> {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error); 
+});
+
+app.use((error, req, res, next)=> {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message,
+        },
+    }); 
+});
 
 module.exports = app;
