2c02d263fa83af866be1b9bc85124b3bb13d30ae
DeleMike <akindelemichael65@gmail.com>
2021-02-12 14:15:11 +0100

    add all routes

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,45 +3,29 @@ const express = require('express');
 //enables us to access this resource "/products"
 const router = express.Router();
 
-//create the GET request method for this route
-//using '/' because products is the default route
 router.get('/', (req, res, next) => {
     res.status(200).json({
-        message: 'Handling GET request to /products'
+        message: 'Orders were fetched',
     });
 });
 
-//POST
 router.post('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling POST request to /products'
+    res.status(201).json({
+        message: 'Order was created',
     });
 });
 
-router.get('/:productId', (req, res, next) => {
-    const id = req.params.productId;
-    if (id === 'special') {
-        res.status(200).json({
-            message: 'You have gotten the special product',
-            id: id,
-        });
-    } else {
-        res.status(200).json({
-            message: 'You got a normal product',
-            id: id,
-        });
-    }
-});
-
-router.patch('/:productId', (req, res, next) => {
+router.get('/:orderId', (req, res, next) => {
     res.status(200).json({
-        message: 'Updated product',
+        message: 'You have gotten an order',
+        id:  req.params.orderId,
     });
 });
 
-router.delete('/:productId', (req, res, next) => {
+router.delete('/:orderId', (req, res, next) => {
     res.status(200).json({
-        message: 'Delete product',
+        message: 'You have deleted an order',
+        id:  req.params.orderId,
     });
 });
 
--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const express = require('express');
 //save all functions that comes with "express" in a variable to access its functions
 const app = express();
 
-app.use((req, res, next) => {
-    res.status(200).json({
-        message : 'It works!'
-    });
-});
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders'); 
+
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
 module.exports = app;
